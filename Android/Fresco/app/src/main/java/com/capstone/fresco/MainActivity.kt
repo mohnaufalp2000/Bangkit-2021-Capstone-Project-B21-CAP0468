b286c6ff68f4d1bfa58818bf5ea1c7c124a10b3e
dedyramadhan <copyninja135@gmail.com>
2021-05-29 21:01:19 +0700

    Create some class for login feature

--- Android/Fresco/app/src/main/java/com/capstone/fresco/MainActivity.kt
+++ Android/Fresco/app/src/main/java/com/capstone/fresco/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.capstone.fresco
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 
 class MainActivity : AppCompatActivity() {
+
+    private var authListener: AuthStateListener? = null
+    private var auth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Logout: Button = findViewById(R.id.logout)
+
+        auth = FirebaseAuth.getInstance()
+
+        //Check if user have been logout
+        authListener =
+            AuthStateListener { firebaseAuth ->
+                val user = firebaseAuth.currentUser
+                if (user == null) {
+                    Toast.makeText(this@MainActivity, "Logout Success", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                    finish()
+                }
+            }
+        Logout.setOnClickListener {
+            auth!!.signOut()
+        }
+    }
+
+    //Add Listener
+    override fun onStart() {
+        super.onStart()
+        auth!!.addAuthStateListener(authListener!!)
+    }
+
+    //Remove Listener
+    override fun onStop() {
+        super.onStop()
+        if (authListener != null) {
+            auth!!.removeAuthStateListener(authListener!!)
+        }
     }
 }
--- Android/Fresco/app/src/main/java/com/capstone/fresco/ui/auth/AuthActivity.kt
+++ Android/Fresco/app/src/main/java/com/capstone/fresco/ui/auth/AuthActivity.kt
@@ -1,8 +1,10 @@
 package com.capstone.fresco.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.capstone.fresco.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
 
@@ -11,5 +13,10 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val logout: Button = findViewById(R.id.logout)
+        logout.setOnClickListener { //Digunakan Untuk Logout
+            FirebaseAuth.getInstance().signOut()
+            finish()
+        }
     }
 }
