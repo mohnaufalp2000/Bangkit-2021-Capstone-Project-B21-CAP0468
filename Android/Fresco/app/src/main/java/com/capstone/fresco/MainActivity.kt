1ac436e06cc79d75339f7b2fc54d4756bc483f9e
dedyramadhan <copyninja135@gmail.com>
2021-05-30 22:32:40 +0700

    Create some class for login feature 2

--- Android/Fresco/app/src/main/java/com/capstone/fresco/MainActivity.kt
+++ Android/Fresco/app/src/main/java/com/capstone/fresco/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.capstone.fresco.ui.auth.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 
